a58bf6f83d2f1762bef0055cde13990295b9a4ac
Breakrice <breakrice621@gmail.com>
2026-07-28 00:55:43 -0500

    started artaria logic

--- scripts/logic.lua
+++ scripts/logic.lua
@@ -1,3 +1,4 @@
+-- "access_rules": []
 function has(item, amount)
     local count = Tracker:ProviderCountForCode(item)
     amount = tonumber(amount)
@@ -12,4 +13,16 @@ function compareCounts(item1, item2)
     local count1 = Tracker:ProviderCountForCode(item1)
     local count2 = Tracker:ProviderCountForCode(item2)
     return count1 >= count2
+end
+
+function CANBLOWUP()
+    return has("morph") and (has("bomb") or has("powerbomb"))
+end
+
+function IBJbeginner()
+    return has("morph") and has("crossbomb") --and beginner
+end
+
+function IBJintermediate()
+    return has("morph") and has("bomb") --and intermediate
 end
